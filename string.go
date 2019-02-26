92d11db6983f26d8b8b424c99fe99647ac478df8
Cai Huicong <caihuicong@guttv.cn>
2019-02-26 13:11:58 +0800

    Update String function ConcatString

--- string.go
+++ string.go
@@ -1,1 +1,11 @@
+package util
 
+import "bytes"
+
+func ConcatString(inString ... string)string{
+	bufferStr := bytes.Buffer{}
+	for _,value := range inString{
+		bufferStr.WriteString(value)
+	}
+	return bufferStr.String()
+}
